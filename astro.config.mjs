0de1f69bc3fcf3f005eccd71bfcab9a61d416fcc
Daniel Bautista P. <76835848+Danielbp019@users.noreply.github.com>
2025-10-15 12:37:37 -0500

    Fix trailingSlash
    trailingSlash: "always". Controla las rutas que terminan en / para eliminar problemas con cloudflare.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  /* Esto controla el error de las rutas terminadas en / de cloudflare */
+  trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()],
   },
