63f18e0f5c68b9beea1d60f610273230fd2514be
Daniel Bautista P. <76835848+Danielbp019@users.noreply.github.com>
2025-10-17 02:26:55 -0500

    New SEO con sitemap y mensaje error buscador
    sitemap y robots agregados para mejorar el SEO. Mensaje de sin resultados en el buscador cuando la búsqueda no coincida.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  // Define la URL base de tu sitio para generar URLs absolutas en el sitemap.
+  site: "https://guiarendergaming.pages.dev",
   /* Esto controla el error de las rutas terminadas en / de cloudflare */
   trailingSlash: "always",
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
